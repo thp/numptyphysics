495ca5dcdd4da542b5e48287c303ac395dc86143
Thomas Perl <m@thp.io>
2015-01-02 17:42:18 +0100

    Import manpage from Debian (Fixes #5)

--- platform/freedesktop/install.mk
+++ platform/freedesktop/install.mk
@@ -1,5 +1,6 @@
 install_freedesktop:
 	install -D -m 644 platform/freedesktop/$(APP).desktop $(DESTDIR)/$(PREFIX)/share/applications/$(APP).desktop
 	install -D -m 644 platform/freedesktop/$(APP).png $(DESTDIR)/$(PREFIX)/share/icons/hicolor/256x256/apps/$(APP).png
+	install -D -m 644 platform/freedesktop/$(APP).6 $(DESTDIR)/$(PREFIX)/share/man/man6/$(APP).6
 ADDITIONAL_INSTALL_TARGETS += install_freedesktop
 .PHONY: install_freedesktop
