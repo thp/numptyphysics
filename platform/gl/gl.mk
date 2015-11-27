d642dd0133e84eea9a92a3d321a908e92fe9f1ff
Thomas Perl <m@thp.io>
2015-11-27 22:09:49 +0100

    Fix gl.mk
    This is not pre-processed with SED (the .in variant
    doesn't work without a harmless warning message), so
    we just use the direct make syntax.
    
    Thanks to Samuel Bauer for the report.

--- platform/gl/gl.mk
+++ platform/gl/gl.mk
@@ -3,9 +3,9 @@ ifeq ($(shell uname),Darwin)
 LIBS += -framework OpenGL
 else
 ifneq ($(wildcard $(patsubst %,%/libGL.so.1,/usr/lib /usr/lib64 /usr/lib/*)),)
-add_pkgconfig(gl)
+$(eval $(call add_pkgconfig,gl))
 else
-add_pkgconfig(glesv2)
+$(eval $(call add_pkgconfig,glesv2))
 CFLAGS += -DUSE_OPENGL_ES
 CXXFLAGS += -DUSE_OPENGL_ES
 endif
