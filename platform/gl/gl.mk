66c07289b5d200e2a2b07c002ee0d340fe14b2a9
Thomas Perl <m@thp.io>
2015-11-27 23:03:55 +0100

    libGL.so.1 search path for Ubuntu 15.10

--- platform/gl/gl.mk
+++ platform/gl/gl.mk
@@ -2,7 +2,7 @@
 ifeq ($(shell uname),Darwin)
 LIBS += -framework OpenGL
 else
-ifneq ($(wildcard $(patsubst %,%/libGL.so.1,/usr/lib /usr/lib64 /usr/lib/*)),)
+ifneq ($(wildcard $(patsubst %,%/libGL.so.1,/usr/lib /usr/lib64 /usr/lib/* /usr/lib/*/mesa)),)
 $(eval $(call add_pkgconfig,gl))
 else
 $(eval $(call add_pkgconfig,glesv2))
